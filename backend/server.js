121078cefc632d801a5fdec96542f0b032ff2744
321999 <kishorejha321999@gmail.com>
2024-01-07 12:53:37 +0530

    required packages are installed

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const exp = require("express")
+// creating instance of the express app
+const app = exp()
+
+// listnening on the port 5000
+app.listen("5000", console.log("server is activated:localhost::/"))
