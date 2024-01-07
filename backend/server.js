494c1847e32905644cbd7f3c0c47b93ae34cc917
321999 <kishorejha321999@gmail.com>
2024-01-07 13:09:10 +0530

    knowonwards used nodemon to run the project

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,18 @@ const exp = require("express")
 // creating instance of the express app
 const app = exp()
 
+app.get("/home", (req, res) => {
+    // Option 1: Send a specific response object
+    res.json({ message: "Welcome home!", name: "kishore jha" });
+
+    // Option 2: Send select properties of the response object
+    const safeRes = {
+        statusCode: res.statusCode,
+        statusMessage: res.statusMessage,
+        headers: res.getHeaders(),
+    };
+    res.json(safeRes);
+});
+
 // listnening on the port 5000
 app.listen("5000", console.log("server is activated:http://localhost:5000/"))
